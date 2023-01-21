7a7aa18293d549b7e7ac9d4294f0ac22ed94487a
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-01-20 23:15:12 -0700

    Changed HTML answer spaces to buttons to enable interaction, added questionairre to JavaScript

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -13,3 +13,34 @@ function setTimer() {
     }
 
     setTimer();
+
+    var questionairre = [{
+
+        id: 0,
+
+        q: "In JavaScript, an Array can store which of the following?",
+        a:[{text:"Integers", isCorrect: false },
+           {text:"Objects", isCorrect: false },
+           {text:"functions", isCorrect: false},
+           {text:"All of the above", isCorrect: true},
+        ]
+    },
+    {
+        id: 1, 
+        q: "Commonly used data types do NOT include:",
+        a: [{text:"Booleans", isCorrect: false },
+            {text:"Strings", isCorrect: false },
+            {text:"Alerts", isCorrect: true},
+            {text:"Numbers", isCorrect: false,}
+    ]
+    },
+    {
+        id: 2, 
+        q: "The condition in an if/else staement is enclosed with in ____.",
+        a: [{text:"Quotes", isCorrect: false },
+            {text:"Curly Brackets", isCorrect: false },
+            {text:"Parentheses", isCorrect: true},
+            {text:"SquareBrackets", isCorrect: false,}
+        ]
+    }
+]
