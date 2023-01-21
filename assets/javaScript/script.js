3919ca680df0b85e68c8ea60cde1dd2f56ac1a2f
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-01-20 21:11:15 -0700

    Reworked CountDown Timer function.

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,21 +1,15 @@
-const startTime = .5;
-var timer = startTime * 60;
 
-const countDownEl = document.getElementById('countdown');
+var timeEl = document.querySelector("#countdown")
+var secondsLeft = 10;
 
-setInterval(updateCountDown, 1000);
+function setTimer() {
+    var timerInterval = setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left!";
+        
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval)};
+        }, 1000); 
+    }
 
-function updateCountDown() {
-    const minutes = Math.floor(timer / 60);
-    let seconds = timer % 60;
-
-    countDownEl.innerHTML = `${minutes}:${seconds}`;
-    timer--;
-     
-    // if (timer == 0) {
-    //     console.log("Times Up!");
-    // // }
-        if (timer === 0) {
-            clearInterval(seconds);
-        }
-}
+    setTimer();
